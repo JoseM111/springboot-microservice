234bcd93db6bd9a39d3ee5d5b458c90c78ef4d6a
Jose Martinez <44812411+JoseM111@users.noreply.github.com>
2022-09-05 11:12:14 -0400

    EPIC0002: completed this section [ Sept 5th ]

--- clients/src/main/java/clients/IHelpers/IHelpers.java
+++ clients/src/main/java/clients/IHelpers/IHelpers.java
@@ -5,7 +5,7 @@ public interface IHelpers {
     static String toStrFormatter(Object... args) {
         return (
                 """
-                        =========================================================
+                        \n=========================================================
                         NotificationClient: {
                           toCustomerId: %s
                           toCustomerName: %s
